9f00b23945ff0ed2256a067ee5db4a8386125ec4
NeronLeVelu <63407078+NeronLeVelu@users.noreply.github.com>
2025-01-12 12:16:44 +0100

    Update imapcopy.py
    bug about some b string on python3 that failed in recurse option

--- imapcopy.py
+++ imapcopy.py
@@ -7,6 +7,7 @@
 
     :copyright: (c) 2013 by Christoph Heer.
     :license: BSD, see LICENSE for more details.
+    :small bug correction for recurse with python3 using bstring
 """
 
 import sys
@@ -14,7 +15,6 @@ import hashlib
 import imaplib
 import logging
 import argparse
-import email
 
 
 class IMAP_Copy(object):
@@ -28,10 +28,10 @@ class IMAP_Copy(object):
         'port': 993
     }
     destination_auth = ()
-    mailbox_mapping = []
+    folder_mapping = []
 
-    def __init__(self, source_server, destination_server, mailbox_mapping,
-                 source_auth=(), destination_auth=(), create_mailboxes=False,
+    def __init__(self, source_server, destination_server, folder_mapping,
+                 source_auth=(), destination_auth=(), create_folders=False,
                  recurse=False, skip=0, limit=0):
 
         self.logger = logging.getLogger("IMAP_Copy")
@@ -41,8 +41,8 @@ class IMAP_Copy(object):
         self.source_auth = source_auth
         self.destination_auth = destination_auth
 
-        self.mailbox_mapping = mailbox_mapping
-        self.create_mailboxes = create_mailboxes
+        self.folder_mapping = folder_mapping
+        self.create_folders = create_folders
 
         self.skip = skip
         self.limit = limit
@@ -66,8 +66,20 @@ class IMAP_Copy(object):
         setattr(self, '_conn_%s' % target, connection)
         self.logger.info("%s connection established" % target)
         # Detecting delimiter on destination server
-        code, mailbox_list = connection.list()
-        self.delimiter = mailbox_list[0].split('"')[1]
+        code, folder_list = connection.list()
+
+        folder_name_list = []
+        for box in folder_list:
+            parts = box.decode('utf-8').split('"')
+            if len(parts) == 5:
+                folder_name_list.append(parts[3].strip())
+            elif len(parts) == 3:
+                folder_name_list.append(parts[2].strip())
+
+        folder_names = ', '.join(folder_name_list)
+        self.logger.info("%s has the following folders: %s" % (target, folder_names))
+
+        self.delimiter = folder_list[0].split(b'"')[1]
 
     def connect(self):
         self._connect('source')
@@ -80,7 +92,7 @@ class IMAP_Copy(object):
         connection = getattr(self, '_conn_%s' % target)
         if connection.state == 'SELECTED':
             connection.close()
-            self.logger.info("Close mailbox on %s" % target)
+            self.logger.info("Close folder on %s" % target)
 
         self.logger.info("Disconnect from %s server" % target)
         connection.logout()
@@ -90,52 +102,40 @@ class IMAP_Copy(object):
         self._disconnect('source')
         self._disconnect('destination')
 
-    def copy(self, source_mailbox, destination_mailbox, skip, limit, recurse_level=0):
-        if self.recurse:
-            self.logger.info("Getting list of mailboxes under %s" % source_mailbox)
-            connection = self._conn_source
-            typ, data = connection.list(source_mailbox)
-            for d in data:
-                if d:
-                    new_source_mailbox = d.split('"')[3]  # Getting submailbox name
-                    if new_source_mailbox.count('/') == recurse_level:
-                        self.logger.info("Recursing into %s" % new_source_mailbox)
-                        new_destination_mailbox = new_source_mailbox.split("/")[recurse_level]
-                        self.copy(new_source_mailbox, destination_mailbox + self.delimiter + new_destination_mailbox,
-                                  skip, limit, recurse_level + 1)
+    def copy(self, source_folder, destination_folder, skip, limit, recurse=True):
 
         # There should be no files stored in / so we are bailing out
-        if source_mailbox == '':
+        if source_folder == '':
             return
 
-        # Connect to source and open mailbox
-        status, data = self._conn_source.select(source_mailbox, True)
+        # Connect to source and open folder
+        self.logger.info( "Try to open source_folder:%s" % ( source_folder) )
+        status, data = self._conn_source.select(source_folder, True)
         if status != "OK":
-            self.logger.error("Couldn't open source mailbox %s" %
-                              source_mailbox)
+            self.logger.error("Couldn't open source folder %s (status: %s [%s])" %
+                              ( source_folder, status, data) )
             sys.exit(2)
 
-        # Connect to destination and open or create mailbox
-        status, data = self._conn_destination.select(destination_mailbox)
-        if status != "OK" and not self.create_mailboxes:
-            self.logger.error("Couldn't open destination mailbox %s" %
-                              destination_mailbox)
+        # Connect to destination and open or create folder
+        status, data = self._conn_destination.select(destination_folder)
+        if status != "OK" and not self.create_folders:
+            self.logger.error("Couldn't open destination folder %s" %
+                              destination_folder)
             sys.exit(2)
         else:
-            self.logger.info("Create destination mailbox %s" %
-                             destination_mailbox)
-            self._conn_destination.create(destination_mailbox)
-            self._conn_destination.subscribe(destination_mailbox)
-            status, data = self._conn_destination.select(destination_mailbox)
+            self.logger.info("Create destination folder %s" %
+                             destination_folder)
+            self._conn_destination.create(destination_folder)
+            status, data = self._conn_destination.select(destination_folder)
 
         # Look for mails
-        self.logger.info("Looking for mails in %s" % source_mailbox)
+        self.logger.info("Looking for mail in %s" % source_folder)
         status, data = self._conn_source.search(None, 'ALL')
         data = data[0].split()
         mail_count = len(data)
 
         self.logger.info("Start copy %s => %s (%d mails)" % (
-            source_mailbox, destination_mailbox, mail_count))
+            source_folder, destination_folder, mail_count))
 
         progress_count = 0
         copy_count = 0
@@ -147,65 +147,133 @@ class IMAP_Copy(object):
                     progress_count, mail_count))
                 continue
             else:
-                status, data = self._conn_source.fetch(msg_num, '(RFC822 FLAGS)')
+                status, data = self._conn_source.fetch(msg_num, '(RFC822 FLAGS INTERNALDATE)')
+
+                flags_line = data[0][0].decode('ascii')
+                if flags_line.find('FLAGS') < 0 and len(data) > 1:
+                    flags_line = data[1].decode('ascii')
                 message = data[0][1]
-                flags = data[1][8:][:-2]  # Not perfect.. Waiting for bug reports
-                msg = email.message_from_string(message);
-                msgDate = email.utils.parsedate(msg['Date'])
+
+                flags_start = flags_line.index('FLAGS (') + len('FLAGS (')
+                flags_end = flags_line.index(')', flags_start)
+
+                flags = '(' + flags_line[flags_start:flags_end] + ')'
+
+                internaldate_start = flags_line.index('INTERNALDATE ') + len('INTERNALDATE ')
+                internaldate_end = flags_line.find(' RFC822', internaldate_start)
+                if internaldate_end < 0:
+                    internaldate_end = flags_line.find(' FLAGS', internaldate_start)
+                if internaldate_end < 0:
+                    internaldate_end = flags_line.find(')', internaldate_start)
+                if internaldate_end < 0:
+                    internaldate_end = len(flags_line)
+
+                internaldate = flags_line[internaldate_start:internaldate_end]
 
                 self._conn_destination.append(
-                    destination_mailbox, flags, msgDate, message
+                    destination_folder, flags, internaldate, message,
                 )
 
                 copy_count += 1
-                message_md5 = hashlib.md5(message).hexdigest()
+                message_sha1 = hashlib.sha1(message).hexdigest()
 
-                self.logger.info("Copy mail %d of %d (copy_count=%d, md5(message)=%s)" % (
-                    progress_count, mail_count, copy_count, message_md5))
+                self.logger.info("Copy mail %d of %d (copy_count=%d, sha1(message)=%s)" % (
+                    progress_count, mail_count, copy_count, message_sha1))
 
                 if limit > 0 and copy_count >= limit:
                     self.logger.info("Copy limit %d reached (copy_count=%d)" % (
                         limit, copy_count))
                     break
 
-        self.logger.info("Copy complete %s => %s (%d out of %d mails copied)" % (
-            source_mailbox, destination_mailbox, copy_count, mail_count))
+        self.logger.info("Copy complete %s => %s (%d out of %d messages copied)" % (
+            source_folder, destination_folder, copy_count, mail_count))
+
+        if self.recurse and recurse:
+            self.logger.info("Getting list of folders under %s" % source_folder)
+            connection = self._conn_source
+            typ, data = connection.list(source_folder)
+            self.logger.info( "data (%s) contain : %s" %( type( data), data) )
+            Parent = True
+            for d in data:
+                if d:
+                    if Parent:
+                       # skip parent that is just copied
+                       Parent = False
+                       continue
+                    self.logger.info( "d (%s) contain : %s" %( type( d), d) )
+                    l_resp = d.split( b'"')
+                    # response = '(\HasChildren) "/" INBOX'
+                    if len(l_resp) == 3:
+
+                        source_mbox = d.split(b'"')[2].strip()
+                        self.logger.info( "source_mbox (%s) contain : %s" %( type( source_mbox), source_mbox) )
+                        # make sure we don't have a recursive loop
+                        if source_mbox != source_folder:
+                            # maybe better use regex to replace only start of the souce name
+                            dest_mbox = source_mbox.replace(source_folder.encode('latin-1'), destination_folder.encode( 'latin-1'))
+                            self.logger.info("starting copy of folder %s to %s " % (source_mbox, dest_mbox))
+                            self.copy(source_mbox, dest_mbox, skip, limit, False)
 
     def run(self):
         try:
             self.connect()
-            for source_mailbox, destination_mailbox in self.mailbox_mapping:
-                self.copy(source_mailbox, destination_mailbox, self.skip, self.limit)
+            for source_folder, destination_folder in self.folder_mapping:
+
+                if ' ' in source_folder and '"' not in source_folder:
+                    source_folder = '"%s"' % source_folder
+                if ' ' in destination_folder and '"' not in destination_folder:
+                    destination_folder = '"%s"' % destination_folder
+
+                self.copy(source_folder, destination_folder, self.skip, self.limit)
+        finally:
+            self.disconnect()
+
+    def test_connections(self):
+        self.logger.info("Testing connections to source and destination")
+        try:
+            self.connect()
+            self.logger.info("Test OK")
+        except Exception as e:
+            self.logger.error("Connection error: %s" % str(e))
         finally:
             self.disconnect()
 
 
 def main():
     parser = argparse.ArgumentParser()
+
     parser.add_argument('source',
-                        help="Source host ex. imap.googlemail.com:993")
+                        help="source host, e.g. imap.googlemail.com:993")
+
     parser.add_argument('source_auth', metavar='source-auth',
-                        help="Source host authentication ex. "
-                             "username@host.de:password")
+                        help="source host credentials, e.g. username@host.de:password")
 
     parser.add_argument('destination',
-                        help="Destination host ex. imap.otherhoster.com:993")
-    parser.add_argument('destination_auth', metavar='destination-auth',
-                        help="Destination host authentication ex. "
-                             "username@host.de:password")
+                        help="destination host, e.g. imap.otherhoster.com:993")
 
-    parser.add_argument('mailboxes', type=str, nargs='+',
-                        help='List of mailboxes alternate between source '
-                             'mailbox and destination mailbox.')
-    parser.add_argument('-c', '--create-mailboxes', dest='create_mailboxes',
+    parser.add_argument('destination_auth', metavar='destination-auth',
+                        help="destination host credentials, e.g. username@host.de:password")
+
+    parser.add_argument('folders', type=str, nargs='*',
+                        help="list of folders, alternating between source folder and destination folder")
+
+    parser.add_argument('-t', '--test', dest='test_connections',
                         action="store_true", default=False,
-                        help='Create the mailboxes on destination')
-    parser.add_argument('-r', '--recurse', dest='recurse', action="store_true",
-                        default=False, help='Recurse into submailboxes')
+                        help="do not copy, only test connections to source and destination")
+
+    parser.add_argument('-c', '--create-folders', dest='create_folders',
+                        action="store_true", default=False,
+                        help="create folders on destination")
+
+    parser.add_argument('-r', '--recurse', dest='recurse',
+                        action="store_true", default=False,
+                        help="recurse into subfolders")
+
     parser.add_argument('-q', '--quiet', action="store_true", default=False,
-                        help='ppsssh... be quiet. (no output)')
+                        help="be quiet, print no output")
+
     parser.add_argument('-v', '--verbose', action="store_true", default=False,
-                        help='more output please (debug level)')
+                        help="print debug-level output")
 
     def check_negative(value):
         ivalue = int(value)
@@ -214,9 +282,10 @@ def main():
         return ivalue
 
     parser.add_argument("-s", "--skip", default=0, metavar="N", type=check_negative,
-                        help='skip the first N message(s)')
+                        help="skip the first N message(s)")
+
     parser.add_argument("-l", "--limit", default=0, metavar="N", type=check_negative,
-                        help='only copy N number of message(s)')
+                        help="only copy at most N message(s)")
 
     args = parser.parse_args()
 
@@ -233,14 +302,18 @@ def main():
     source_auth = tuple(args.source_auth.split(':'))
     destination_auth = tuple(args.destination_auth.split(':'))
 
-    if len(args.mailboxes) % 2 != 0:
-        print "Not valid count of mailboxes!"
-        sys.exit(1)
+    if not args.test_connections:
+        if len(args.folders) < 2:
+            print("Missing folders")
+            sys.exit(1)
+        elif len(args.folders) % 2 != 0:
+            print("Please provide an even number of folders")
+            sys.exit(1)
 
-    mailbox_mapping = zip(args.mailboxes[::2], args.mailboxes[1::2])
+    folder_mapping = list(zip(args.folders[::2], args.folders[1::2]))
 
-    imap_copy = IMAP_Copy(source, destination, mailbox_mapping, source_auth,
-                          destination_auth, create_mailboxes=args.create_mailboxes,
+    imap_copy = IMAP_Copy(source, destination, folder_mapping, source_auth,
+                          destination_auth, create_folders=args.create_folders,
                           recurse=args.recurse, skip=args.skip, limit=args.limit)
 
     streamHandler = logging.StreamHandler()
@@ -256,7 +329,10 @@ def main():
         imap_copy.logger.setLevel(logging.DEBUG)
 
     try:
-        imap_copy.run()
+        if args.test_connections:
+            imap_copy.test_connections()
+        else:
+            imap_copy.run()
     except KeyboardInterrupt:
         imap_copy.disconnect()
 
